5ad6beb2f24f7f434cd01b8b278d3e002691c645
Imran Yusuff <imranyusuff@ukm.edu.my>
2025-03-12 13:34:55 +0800

    Changed coordinate system (+Z up)

--- src/HPGeUKMSNDetectorConstruction.cc
+++ src/HPGeUKMSNDetectorConstruction.cc
@@ -95,63 +95,42 @@ G4VPhysicalVolume *HPGeUKMSNDetectorConstruction::DefineVolumes()
 
   G4Box *worldS = new G4Box("world", worldSizeX/2, worldSizeY/2, worldSizeZ/2);
   G4LogicalVolume *worldLV = new G4LogicalVolume(worldS, air, "World");
-  G4VPhysicalVolume *worldPV = new G4PVPlacement(0, G4ThreeVector(), worldLV, "World", 0, false, fCheckOverlaps);
+  G4VPhysicalVolume *worldPV = new G4PVPlacement(nullptr, G4ThreeVector(), worldLV, "World", 0, false, fCheckOverlaps);
 
   G4Tubs *baseshieldS = new G4Tubs("baseShield", baseShieldInnerRadius, shieldRadius, baseShieldThickness/2, 0.*deg, 360.*deg);
   G4LogicalVolume *baseshieldLV = new G4LogicalVolume(baseshieldS, fStopMaterial, "BaseShield");
-  G4RotationMatrix baseshieldRM = G4RotationMatrix();
-  baseshieldRM.rotateX(90.*deg);
-  G4ThreeVector baseshield3V = G4ThreeVector(0, baseShieldThickness/2, 0);
-  G4Transform3D baseshieldTR = G4Transform3D(baseshieldRM, baseshield3V);
-  G4VPhysicalVolume *baseshieldPV = new G4PVPlacement(baseshieldTR, baseshieldLV, "BaseShield", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector baseshieldPos = G4ThreeVector(0, 0, baseShieldThickness/2);
+  G4VPhysicalVolume *baseshieldPV = new G4PVPlacement(nullptr, baseshieldPos, baseshieldLV, "BaseShield", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *bodyshieldS = new G4Tubs("bodyShield", bodyShieldInnerRadius, shieldRadius, bodyShieldHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *bodyshieldLV = new G4LogicalVolume(bodyshieldS, fStopMaterial, "BodyShield");
-  G4RotationMatrix bodyshieldRM = G4RotationMatrix();
-  bodyshieldRM.rotateX(90.*deg);
-  G4ThreeVector bodyshield3V = G4ThreeVector(0, baseShieldThickness + bodyShieldHeight/2, 0);
-  G4Transform3D bodyshieldTR = G4Transform3D(bodyshieldRM, bodyshield3V);
-  G4VPhysicalVolume *bodyshieldPV = new G4PVPlacement(bodyshieldTR, bodyshieldLV, "BodyShield", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector bodyshieldPos = G4ThreeVector(0, 0, baseShieldThickness + bodyShieldHeight/2);
+  G4VPhysicalVolume *bodyshieldPV = new G4PVPlacement(nullptr, bodyshieldPos, bodyshieldLV, "BodyShield", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *doorshieldS = new G4Tubs("doorShield", doorShieldInnerRadius, shieldRadius, doorShieldThickness/2, 0.*deg, 360.*deg);
   G4LogicalVolume *doorshieldLV = new G4LogicalVolume(doorshieldS, fStopMaterial, "DoorShield");
-  G4RotationMatrix doorshieldRM = G4RotationMatrix();
-  doorshieldRM.rotateX(90.*deg);
-  G4ThreeVector doorshield3V = G4ThreeVector(0, baseShieldThickness + bodyShieldHeight + doorShieldThickness/2, 0);
-  G4Transform3D doorshieldTR = G4Transform3D(doorshieldRM, doorshield3V);
-  G4VPhysicalVolume *doorshieldPV = new G4PVPlacement(doorshieldTR, doorshieldLV, "DoorShield", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector doorshieldPos = G4ThreeVector(0, 0, baseShieldThickness + bodyShieldHeight + doorShieldThickness/2);
+  G4VPhysicalVolume *doorshieldPV = new G4PVPlacement(nullptr, doorshieldPos, doorshieldLV, "DoorShield", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *detS = new G4Tubs("detector", 0, detectorRadius, detectorHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *detLV = new G4LogicalVolume(detS, fDetMaterial, "Detector");
-  G4RotationMatrix detRM = G4RotationMatrix();
-  detRM.rotateX(90.*deg);
-  G4ThreeVector det3V = G4ThreeVector(0, detectorHolderHeight - detectorHeight/2, 0);
-  G4Transform3D detTR = G4Transform3D(detRM, det3V);
-  G4VPhysicalVolume *detPV = new G4PVPlacement(detTR, detLV, "Detector", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector detPos = G4ThreeVector(0, 0, detectorHolderHeight - detectorHeight/2);
+  G4VPhysicalVolume *detPV = new G4PVPlacement(nullptr, detPos, detLV, "Detector", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *detHolderS = new G4Tubs("detectorHolder", detectorHolderInnerRadius, detectorHolderOuterRadius, detectorHolderHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *detHolderLV = new G4LogicalVolume(detHolderS, fDetHolderMaterial, "DetectorHolder");
-  G4RotationMatrix detHolderRM = G4RotationMatrix();
-  detHolderRM.rotateX(90.*deg);
-  G4ThreeVector detHolder3V = G4ThreeVector(0, detectorHolderHeight/2, 0);
-  G4Transform3D detHolderTR = G4Transform3D(detHolderRM, detHolder3V);
-  G4VPhysicalVolume *detHolderPV = new G4PVPlacement(detHolderTR, detHolderLV, "DetectorHolder", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector detHolderPos = G4ThreeVector(0, 0, detectorHolderHeight/2);
+  G4VPhysicalVolume *detHolderPV = new G4PVPlacement(nullptr, detHolderPos, detHolderLV, "DetectorHolder", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *endcapS = new G4Tubs("endcap", endcapOuterRadius - endcapThickness, endcapOuterRadius, endcapHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *endcapLV = new G4LogicalVolume(endcapS, fEndcapMaterial, "Endcap");
-  G4RotationMatrix endcapRM = G4RotationMatrix();
-  endcapRM.rotateX(90.*deg);
-  G4ThreeVector endcap3V = G4ThreeVector(0, baseShieldThickness + endcapHeight/2, 0);
-  G4Transform3D endcapTR = G4Transform3D(endcapRM, endcap3V);
-  G4VPhysicalVolume *endcapPV = new G4PVPlacement(endcapTR, endcapLV, "Endcap", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector endcapPos = G4ThreeVector(0, 0, baseShieldThickness + endcapHeight/2);
+  G4VPhysicalVolume *endcapPV = new G4PVPlacement(nullptr, endcapPos, endcapLV, "Endcap", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *endcapTopS = new G4Tubs("endcapTop", 0, endcapOuterRadius, endcapTopThickness/2, 0.*deg, 360.*deg);
   G4LogicalVolume *endcapTopLV = new G4LogicalVolume(endcapTopS, fEndcapTopMaterial, "EndcapTop");
-  G4RotationMatrix endcapTopRM = G4RotationMatrix();
-  endcapTopRM.rotateX(90.*deg);
-  G4ThreeVector endcapTop3V = G4ThreeVector(0, baseShieldThickness + endcapHeight + endcapTopThickness/2, 0);
-  G4Transform3D endcapTopTR = G4Transform3D(endcapTopRM, endcapTop3V);
-  G4VPhysicalVolume *endcapTopPV = new G4PVPlacement(endcapTopTR, endcapTopLV, "EndcapTop", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector endcapTopPos = G4ThreeVector(0, 0, baseShieldThickness + endcapHeight + endcapTopThickness/2);
+  G4VPhysicalVolume *endcapTopPV = new G4PVPlacement(nullptr, endcapTopPos, endcapTopLV, "EndcapTop", worldLV, false, 0, fCheckOverlaps);
 
   if (fGeometrySelection == 0) {
     DefineExperimentGeometry0(worldLV, baseShieldThickness, endcapHeight, endcapTopThickness);
@@ -201,27 +180,18 @@ void HPGeUKMSNDetectorConstruction::DefineExperimentGeometry0(
 
   G4Tubs *srcS = new G4Tubs("source", 0, sourceRadius, sourceHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *srcLV = new G4LogicalVolume(srcS, fSrcMaterial, "Source");
-  G4RotationMatrix srcRM = G4RotationMatrix();
-  srcRM.rotateX(90.*deg);
-  G4ThreeVector src3V = G4ThreeVector(0, (baseShieldThickness + endcapHeight + endcapTopThickness + fSrcBaseDistance + sourceHeight/2), 0);
-  G4Transform3D srcTR = G4Transform3D(srcRM, src3V);
-  fSrcPV = new G4PVPlacement(srcTR, srcLV, "Source", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector srcPos = G4ThreeVector(0, 0, (baseShieldThickness + endcapHeight + endcapTopThickness + fSrcBaseDistance + sourceHeight/2));
+  fSrcPV = new G4PVPlacement(nullptr, srcPos, srcLV, "Source", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *cylStandS = new G4Tubs("cylindricalStand", cylStandOuterRadius - cylStandThickness, cylStandOuterRadius, cylStandHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *cylStandLV = new G4LogicalVolume(cylStandS, fStandMaterial, "CylindricalStand");
-  G4RotationMatrix cylStandRM = G4RotationMatrix();
-  cylStandRM.rotateX(90.*deg);
-  G4ThreeVector cylStand3V = G4ThreeVector(0, baseShieldThickness + endcapHeight + endcapTopThickness + cylStandHeight/2, 0);
-  G4Transform3D cylStandTR = G4Transform3D(cylStandRM, cylStand3V);
-  new G4PVPlacement(cylStandTR, cylStandLV, "CylindricalStand", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cylStandPos = G4ThreeVector(0, 0, baseShieldThickness + endcapHeight + endcapTopThickness + cylStandHeight/2);
+  new G4PVPlacement(nullptr, cylStandPos, cylStandLV, "CylindricalStand", worldLV, false, 0, fCheckOverlaps);
 
   G4Tubs *cylSrcHolderS = new G4Tubs("cylSrcHolder", 0, cylStandOuterRadius - cylStandThickness, cylSrcHolderThickness/2, 0.*deg, 360.*deg);
   G4LogicalVolume *cylSrcHolderLV = new G4LogicalVolume(cylSrcHolderS, fStandMaterial, "CylStandSrcHolder");
-  G4RotationMatrix cylSrcHolderRM = G4RotationMatrix();
-  cylSrcHolderRM.rotateX(90.*deg);
-  G4ThreeVector cylSrcHolder3V = G4ThreeVector(0, baseShieldThickness + endcapHeight + endcapTopThickness + fSrcBaseDistance - cylSrcHolderThickness/2, 0);
-  G4Transform3D cylSrcHolderTR = G4Transform3D(cylSrcHolderRM, cylSrcHolder3V);
-  fCylSrcHolderPV = new G4PVPlacement(cylSrcHolderTR, cylSrcHolderLV, "CylStandSrcHolder", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cylSrcHolderPos = G4ThreeVector(0, 0, baseShieldThickness + endcapHeight + endcapTopThickness + fSrcBaseDistance - cylSrcHolderThickness/2);
+  fCylSrcHolderPV = new G4PVPlacement(nullptr, cylSrcHolderPos, cylSrcHolderLV, "CylStandSrcHolder", worldLV, false, 0, fCheckOverlaps);
 
   G4VisAttributes yellow(G4Colour::Yellow());
   G4VisAttributes green(G4Colour::Green());
@@ -253,51 +223,33 @@ void HPGeUKMSNDetectorConstruction::DefineExperimentGeometry1(
 
   G4Tubs *srcS = new G4Tubs("source", 0, sourceRadius, sourceHeight/2, 0.*deg, 360.*deg);
   G4LogicalVolume *srcLV = new G4LogicalVolume(srcS, fSrcMaterial, "Source");
-  G4RotationMatrix srcRM = G4RotationMatrix();
-  srcRM.rotateX(90.*deg);
-  G4ThreeVector src3V = G4ThreeVector(0, (baseShieldThickness + fSrcBaseDistance + sourceHeight/2), 0);
-  G4Transform3D srcTR = G4Transform3D(srcRM, src3V);
-  fSrcPV = new G4PVPlacement(srcTR, srcLV, "Source", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector srcPos = G4ThreeVector(0, 0, (baseShieldThickness + fSrcBaseDistance + sourceHeight/2));
+  fSrcPV = new G4PVPlacement(nullptr, srcPos, srcLV, "Source", worldLV, false, 0, fCheckOverlaps);
 
   G4Box *cubStandSideS = new G4Box("cubStandSide", cubStandThickness/2, cubStandLength/2, cubStandHeight/2); //for both sides
 
   G4LogicalVolume *cubStandSide1LV = new G4LogicalVolume(cubStandSideS, fStandMaterial, "CubicalStandSide1");
-  G4RotationMatrix cubStandSide1RM = G4RotationMatrix();
-  cubStandSide1RM.rotateX(90.*deg);
-  G4ThreeVector cubStandSide13V = G4ThreeVector(cubStandLength/2 - cubStandThickness/2, baseShieldThickness + cubStandHeight/2, 0);
-  G4Transform3D cubStandSide1TR = G4Transform3D(cubStandSide1RM, cubStandSide13V);
-  new G4PVPlacement(cubStandSide1TR, cubStandSide1LV, "CubicalStandSide1", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cubStandSide1Pos = G4ThreeVector(cubStandLength/2 - cubStandThickness/2, 0, baseShieldThickness + cubStandHeight/2);
+  new G4PVPlacement(nullptr, cubStandSide1Pos, cubStandSide1LV, "CubicalStandSide1", worldLV, false, 0, fCheckOverlaps);
 
   G4LogicalVolume *cubStandSide2LV = new G4LogicalVolume(cubStandSideS, fStandMaterial, "CubicalStandSide2");
-  G4RotationMatrix cubStandSide2RM = G4RotationMatrix();
-  cubStandSide2RM.rotateX(90.*deg);
-  G4ThreeVector cubStandSide23V = G4ThreeVector(-(cubStandLength/2 - cubStandThickness/2), baseShieldThickness + cubStandHeight/2, 0);
-  G4Transform3D cubStandSide2TR = G4Transform3D(cubStandSide2RM, cubStandSide23V);
-  new G4PVPlacement(cubStandSide2TR, cubStandSide2LV, "CubicalStandSide2", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cubStandSide2Pos = G4ThreeVector(-(cubStandLength/2 - cubStandThickness/2), 0, baseShieldThickness + cubStandHeight/2);
+  new G4PVPlacement(nullptr, cubStandSide2Pos, cubStandSide2LV, "CubicalStandSide2", worldLV, false, 0, fCheckOverlaps);
 
   G4Box *cubStandBackS = new G4Box("cubStandBack", cubStandLength/2, cubStandBackThickness/2, cubStandHeight/2);
   G4LogicalVolume *cubStandBackLV = new G4LogicalVolume(cubStandBackS, fStandMaterial, "CubicalStandBack");
-  G4RotationMatrix cubStandBackRM = G4RotationMatrix();
-  cubStandBackRM.rotateX(90.*deg);
-  G4ThreeVector cubStandBack3V = G4ThreeVector(0, baseShieldThickness + cubStandHeight/2, -(cubStandLength/2 + cubStandBackThickness/2));
-  G4Transform3D cubStandBackTR = G4Transform3D(cubStandBackRM, cubStandBack3V);
-  new G4PVPlacement(cubStandBackTR, cubStandBackLV, "CubicalStandBack", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cubStandBackPos = G4ThreeVector(0, cubStandLength/2 + cubStandBackThickness/2, baseShieldThickness + cubStandHeight/2);
+  new G4PVPlacement(nullptr, cubStandBackPos, cubStandBackLV, "CubicalStandBack", worldLV, false, 0, fCheckOverlaps);
 
   G4Box *cubStandBarS = new G4Box("cubStandBar", cubStandLength/2, cubStandBackThickness/2, cubStandBarHeight/2);
   G4LogicalVolume *cubStandBarLV = new G4LogicalVolume(cubStandBarS, fStandMaterial, "CubicalStandBar");
-  G4RotationMatrix cubStandBarRM = G4RotationMatrix();
-  cubStandBarRM.rotateX(90.*deg);
-  G4ThreeVector cubStandBar3V = G4ThreeVector(0, baseShieldThickness + cubStandBarHeight/2, cubStandLength/2 + cubStandBackThickness/2);
-  G4Transform3D cubStandBarTR = G4Transform3D(cubStandBarRM, cubStandBar3V);
-  new G4PVPlacement(cubStandBarTR, cubStandBarLV, "CubicalStandBar", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cubStandBarPos = G4ThreeVector(0, -(cubStandLength/2 + cubStandBackThickness/2), baseShieldThickness + cubStandBarHeight/2);
+  new G4PVPlacement(nullptr, cubStandBarPos, cubStandBarLV, "CubicalStandBar", worldLV, false, 0, fCheckOverlaps);
 
   G4Box *cubSrcHolderS = new G4Box("cubSrcHolder", cubStandLength/2, cubStandLength/2, cubSrcHolderThickness/2);
   G4LogicalVolume *cubSrcHolderLV = new G4LogicalVolume(cubSrcHolderS, fStandMaterial, "CubStandSrcHolder");
-  G4RotationMatrix cubSrcHolderRM = G4RotationMatrix();
-  cubSrcHolderRM.rotateX(90.*deg);
-  G4ThreeVector cubSrcHolder3V = G4ThreeVector(0, baseShieldThickness + cubStandHeight + cubSrcHolderThickness/2, 0);
-  G4Transform3D cubSrcHolderTR = G4Transform3D(cubSrcHolderRM, cubSrcHolder3V);
-  new G4PVPlacement(cubSrcHolderTR, cubSrcHolderLV, "CubStandSrcHolder", worldLV, false, 0, fCheckOverlaps);
+  G4ThreeVector cubSrcHolderPos = G4ThreeVector(0, 0, baseShieldThickness + cubStandHeight + cubSrcHolderThickness/2);
+  new G4PVPlacement(nullptr, cubSrcHolderPos, cubSrcHolderLV, "CubStandSrcHolder", worldLV, false, 0, fCheckOverlaps);
 
   G4VisAttributes yellow(G4Colour::Yellow());
   G4VisAttributes green(G4Colour::Green());
@@ -330,9 +282,9 @@ void HPGeUKMSNDetectorConstruction::SetSourceBaseDistance(G4double distance)
   const double oldDistance = fSrcBaseDistance;
   fSrcBaseDistance = distance;
   fSrcPV->SetTranslation(G4ThreeVector(
-    0, fSrcPV->GetTranslation().y() - oldDistance + fSrcBaseDistance, 0));
+    0, 0, fSrcPV->GetTranslation().z() - oldDistance + fSrcBaseDistance));
   fCylSrcHolderPV->SetTranslation(G4ThreeVector(
-    0, fCylSrcHolderPV->GetTranslation().y() - oldDistance + fSrcBaseDistance, 0));
+    0, 0, fCylSrcHolderPV->GetTranslation().z() - oldDistance + fSrcBaseDistance));
 
   // tell G4RunManager that we change the geometry
   G4RunManager::GetRunManager()->GeometryHasBeenModified();
