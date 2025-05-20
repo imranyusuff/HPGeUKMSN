662270f4f6e2ce57c94dbb28f77b05e8fe14039d
Imran Yusuff <imranyusuff@gmail.com>
2025-05-20 22:53:42 +0800

    Geometry #2: Implement radioactive isotopes in soil

--- include/HPGeUKMSNPrimaryGeneratorAction.hh
+++ include/HPGeUKMSNPrimaryGeneratorAction.hh
@@ -1,6 +1,7 @@
 #ifndef HPGeUKMSNPrimaryGeneratorAction_h
 #define HPGeUKMSNPrimaryGeneratorAction_h 1
 
+#include <vector>
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "G4Types.hh"
 
@@ -22,6 +23,12 @@ class HPGeUKMSNPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
   private:
     G4ParticleGun *fParticleGun;
     G4int          fGeometrySelection;
+
+    std::vector<G4int> fIAEA375IsotopesZ;
+    std::vector<G4int> fIAEA375IsotopesA;
+    std::vector<G4double> fIAEA375IsotopesI;
+    std::vector<G4double> fIAEA375IsotopesC;
+    G4double fIAEA375IsotopesSumI;
 };
 
 
--- src/HPGeUKMSNPrimaryGeneratorAction.cc
+++ src/HPGeUKMSNPrimaryGeneratorAction.cc
@@ -4,6 +4,7 @@
 #include "G4ParticleGun.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
+#include "G4IonTable.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4PhysicalVolumeStore.hh"
 #include "G4VPhysicalVolume.hh"
@@ -25,6 +26,29 @@ HPGeUKMSNPrimaryGeneratorAction::HPGeUKMSNPrimaryGeneratorAction(G4int geometryS
   fParticleGun->SetParticleDefinition(particleDefinition);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(1., 0., 0.));
   fParticleGun->SetParticleEnergy(0.661657 * MeV);
+
+  // if soil geometry then populate constituent isotope table
+  if (fGeometrySelection == 2 ||
+      fGeometrySelection == 2000 ||
+      fGeometrySelection == 2001 ||
+      fGeometrySelection == 2002) {
+    // Information from https://analytical-reference-materials.iaea.org/iaea-375
+    // Reference decay rates at 1991-12-31  (TODO: extrapolate to desired date)
+    //                     Cs   Cs   Sr    K   Ra   Th   Th    U    U   Pu   Pu   Am   Sb   Ru    I
+    fIAEA375IsotopesZ = {  55,  55,  38,  19,  88,  90,  90,  92,  92,  94,  94,  95,  51,  44,  53 };
+    fIAEA375IsotopesA = { 137, 134,  90,  40, 226, 228, 232, 234, 238, 239, 238, 241, 125, 106, 129 };
+    fIAEA375IsotopesI = {5280, 463, 108, 424,  20,  21, 20.5, 25, 24.4, 0.3, 0.071, 0.13, 77, 56, 0.0017};
+    fIAEA375IsotopesSumI = 0.0;
+    // find activity sum and build cumulative activity
+    for (auto activity : fIAEA375IsotopesI) {
+      fIAEA375IsotopesSumI += activity;
+      fIAEA375IsotopesC.push_back(fIAEA375IsotopesSumI);
+    }
+    // normalize cumulatives
+    for (unsigned int i = 0; i < fIAEA375IsotopesC.size(); ++i) {
+      fIAEA375IsotopesC[i] /= fIAEA375IsotopesSumI;
+    }
+  }
 }
 
 
@@ -36,6 +60,24 @@ HPGeUKMSNPrimaryGeneratorAction::~HPGeUKMSNPrimaryGeneratorAction()
 
 void HPGeUKMSNPrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent)
 {
+  // Randomize source isotope for soil geometries
+  if (fGeometrySelection == 2 ||
+      fGeometrySelection == 2000 ||
+      fGeometrySelection == 2001 ||
+      fGeometrySelection == 2002) {
+    G4IonTable* ionTable = G4IonTable::GetIonTable();
+    G4double rand = G4UniformRand();
+    G4ParticleDefinition* ion = nullptr;
+    // pick which isotope will decay
+    const int iMax = fIAEA375IsotopesC.size() - 1;
+    int iDecay = -1;
+    do rand -= fIAEA375IsotopesC[++iDecay];
+    while (rand > 0. && iDecay < iMax);
+    ion = ionTable->GetIon(fIAEA375IsotopesZ[iDecay], fIAEA375IsotopesA[iDecay], 0.0);
+    fParticleGun->SetParticleDefinition(ion);
+    fParticleGun->SetParticleEnergy(0.0);  // Energy is ignored for decaying ions
+  }
+
   // Isotropic gamma ray photon direction
   G4double theta = std::acos(2. * G4UniformRand() - 1.0);
   G4double phi = 2. * CLHEP::pi * G4UniformRand();
